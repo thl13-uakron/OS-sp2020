a9e86216d20a8f3554ee8e428b3a04331885a3df
thl13-uakron <thl13@zips.uakron.edu>
2020-01-29 11:21:16 -0500

    Add code for Lab 2 up to Step 2

--- OS2020/kernel.c
+++ OS2020/kernel.c
@@ -27,12 +27,24 @@
 void handleInterrupt21(int,int,int,int);
 void printLogo();
 void clearScreen();
+void readString(char*);
+int mod(int, int);
+int div(int, int);
 
 void main()
 {
+   char input[256];
+
    makeInterrupt21();
    printLogo();
    interrupt(33, 0, "Hello world from Thomas.\r\n\0", 1, 0);
+
+   /* run test function for user input */
+   interrupt(33, 0, "Enter a string: \0", 0, 0);
+   readString(input);
+   interrupt(33, 0, "\nYou entered: \0", 0, 0);
+   interrupt(33, 0, input, 0, 0);
+
    while(1); /* stop program by putting in an infinite loop */
 }
 
@@ -90,7 +102,43 @@ void clearScreen() {
   }
 }
 
+void readString(char* c) {
+  int i = 0; /* index to store character */
+  char enter = 13; /* ascii code of enter character */
+  char backspace = 8; /* ascii code of backspace character */
 
+  /* record input */
+  char* input;
+  input[1] = '\0';
+
+  while (1) {
+    /* take and print character from keyboard input */
+    input[0] = interrupt(22, 0, 0, 0, 0);
+    interrupt(33, 0, input, 0, 0);
+
+    /* terminate input loop if enter is pressed */
+    if (input[0] == enter) {
+      /* append null character to string */
+      c[i] = '\0';
+      break;
+    }
+    else {
+      /* record character to string otherwise */
+      c[i] = input[0];
+
+      /* move to next index unless backspace is pressed */
+      if (c[i] == backspace) {
+        /* decrement index if backspace is pressed unless index is 0 */
+        if (i > 0) i = i - 1;
+      }
+      else {
+        i = i + 1;
+      }
+    }
+  }
+
+  return;
+}
 /* ^^^^^^^^^^^^^^^^^^^^^^^^ */
 /* MAKE FUTURE UPDATES HERE */
 
@@ -101,9 +149,11 @@ void handleInterrupt21(int ax, int bx, int cx, int dx)
    switch(ax) {
       case 0:
         /* if ax is 0, bx gets printed with cx determining if the output goes to the screen or the printer */
-        printString(bx, cx); 
+        printString(bx, cx);
         break;
-/*      case 1: case 2: case 3: case 4: case 5: */
+      case 1:
+        break;
+/*      case 2: case 3: case 4: case 5: */
 /*      case 6: case 7: case 8: case 9: case 10: */
 /*      case 11: case 12: case 13: case 14: case 15: */
       default: interrupt(33, 0, "General BlackDOS error.\r\n\0", 0, 0);
